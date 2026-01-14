dff3fa31cf6a38b0668085ea87365f16d583ff2d
Eduardo Edson Batista Cordeiro Alves <eduardo.alves.94@hotmail.com>
2026-01-14 11:32:16 +0000

    Add blog, cash shop, streamer, and NPC modules along with related CRUD operations and file organization for CMS integration

---
+++ data/logs/paypal.log.php
@@ -0,0 +1,34 @@
+<?php exit('Forbidden'); ?>
+[2026-01-01 21:57:49] Received notification from 173.0.80.116 (173.0.80.116)
+[2026-01-01 21:57:49] Proceeding to validate the authenticity of the transaction...
+[2026-01-01 21:57:49] Transaction identified as 7XJ98217A5531132G.
+[2026-01-01 21:57:49] Received 1.00 (USD).
+[2026-01-01 21:57:49] Deposited into PayPal account: 0.55 EUR.
+[2026-01-01 21:57:49] Game server name: RetRO, account ID: 2000001
+[2026-01-01 21:57:49] Payment for txn_id#7XJ98217A5531132G has been completed.
+[2026-01-01 21:57:49] Identified as first-time donation to the server from this account.
+[2026-01-01 21:57:49] Updating account credit balance from 0 to 1000
+[2026-01-01 21:57:49] Deposited credits.
+[2026-01-01 21:57:49] Saving transaction details for 7XJ98217A5531132G...
+[2026-01-01 21:57:49] Failed to save transaction details for 7XJ98217A5531132G to file.
+[2026-01-01 21:57:49] Done processing 7XJ98217A5531132G.
+[2026-01-01 22:00:44] Received notification from 173.0.81.65 (notify.paypal.com)
+[2026-01-01 22:00:44] Query string: cmd=_notify-validate&mc_gross=1.00&settle_amount=0.54&protection_eligibility=Eligible&address_status=unconfirmed&payer_id=7ZFJPEUBFBKUJ&address_street=Quadra+104%2C+703++Norte+%28%C3%81guas+Claras%29&payment_date=14%3A00%3A36+Jan+01%2C+2026+PST&payment_status=Completed&charset=UTF-8&address_zip=71909-180&first_name=Eduardo&mc_fee=0.35&address_country_code=BR&exchange_rate=0.824769&address_name=Eduardo+Edson&notify_version=3.9&settle_currency=EUR&custom=YToyOntzOjExOiJzZXJ2ZXJfbmFtZSI7czo1OiJSZXRSTyI7czoxMDoiYWNjb3VudF9pZCI7aToyMDAwMDAxO30%3D&payer_status=verified&business=paypalpt%40eduardoalves.tech&address_country=Brazil&address_city=Bras%C3%ADlia&quantity=1&verify_sign=AiBw2TLzkWd5TqDdXDvMhds0EzKfAUsXzdj69c3qX1V3HM9Abi8-EO4v&payer_email=paypal%40eduardoalves.tech&txn_id=8YP062172Y554544L&payment_type=instant&last_name=Edson&address_state=DF&receiver_email=paypalpt%40eduardoalves.tech&payment_fee=0.35&shipping_discount=0.00&insurance_amount=0.00&receiver_id=X2MB6CTQRV56L&txn_type=web_accept&item_name=Donation+Credits%3A+1%2C000+CREDIT%28s%29&discount=0.00&mc_currency=USD&item_number=&residence_country=BR&shipping_method=Default&transaction_subject=YToyOntzOjExOiJzZXJ2ZXJfbmFtZSI7czo1OiJSZXRSTyI7czoxMDoiYWNjb3VudF9pZCI7aToyMDAwMDAxO30%3D&payment_gross=1.00&ipn_track_id=c9b754a5aeb5c
+[2026-01-01 22:00:44] Establishing connection to PayPal server at www.paypal.com:443...
+[2026-01-01 22:00:44] Connected. Sending request back to PayPal...
+[2026-01-01 22:00:44] Sent 1304 bytes of transaction data. Request size: 1449 bytes.
+[2026-01-01 22:00:44] Reading back response from PayPal...
+[2026-01-01 22:00:44] Notification verified. (recv: VERIFIED)
+[2026-01-01 22:00:44] Proceeding to validate the authenticity of the transaction...
+[2026-01-01 22:00:44] Transaction identified as 8YP062172Y554544L.
+[2026-01-01 22:00:44] Received 1.00 (USD).
+[2026-01-01 22:00:44] Deposited into PayPal account: 0.54 EUR.
+[2026-01-01 22:00:44] Game server name: RetRO, account ID: 2000001
+[2026-01-01 22:00:44] Payment for txn_id#8YP062172Y554544L has been completed.
+[2026-01-01 22:00:44] Updating account credit balance from 1000 to 2000
+[2026-01-01 22:00:44] Deposited credits.
+[2026-01-01 22:00:44] Saving transaction details to PayPal transactions table...
+[2026-01-01 22:00:44] Stored information in PayPal transactions table for server RetRO.
+[2026-01-01 22:00:44] Saving transaction details for 8YP062172Y554544L...
+[2026-01-01 22:00:44] Failed to save transaction details for 8YP062172Y554544L to file.
+[2026-01-01 22:00:44] Done processing 8YP062172Y554544L.
